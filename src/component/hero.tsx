7f11d651796f35bf023c6a537c9452e9811a6e9c
Parinya-cram <parinya.pat@rmutto.ac.th>
2025-11-03 14:30:28 +0700

    Add Hero component with background image

---
+++ src/component/hero.tsx
@@ -0,0 +1,10 @@
+import tailwindcss from 'tailwindcss';
+// import { motion } from "framer-motion";
+
+export default function Hero() {
+    return (
+        <div>
+            <img src="/public/background/BGhero.png" alt="Hero Image" className="w-full h-full" />
+        </div>
+    );
+};
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,12 +1,18 @@
 import Menu from "../component/menu";
+import Hero from "../component/hero";
 import Footer from "../component/footer";
 
 export default function Home() {
     return (
     <div className="flex flex-col min-h-screen">
-      <main className="flex-grow">
-        <Menu />
+      <main className="flex flex-col h-auto">
+        <div className="flex items-center justify-between p-10 lg:px-8">
+          <Menu />
+        </div>
       </main>
+      <div>
+        <Hero />
+      </div>
       <Footer />
     </div>
     );
